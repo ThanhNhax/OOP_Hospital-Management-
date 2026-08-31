95f2703535351ea13a08a21dca5ddcd3a16dfeec
philinh6000 <philinh6000@gmail.com>
2026-08-31 15:18:04 -0700

    - Tạo class Doctor kế thừa BaseEntity bổ sung sex address departmentID và override showInfo - Sửa Department: Bổ sung constructor rỗng, sửa getName thành getDepartmentName để rõ nghĩa hơn khi lấy (name có tên khoa và tên bác sĩ khác nhau) - Viết xong class IManager<T>

--- src/main/java/hospital/Doctor.java
+++ src/main/java/hospital/Doctor.java
@@ -1,17 +1,50 @@
 package hospital;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
-public class Doctor extends BaseEntity {
+public class Doctor extends BaseEntity{
+    private String sex;
+    private String address;
+    private String departmentID; // Vì doctorID là chính -> dùng kế thừa từ base -> cần tạo mới deptID (phụ) trong class này.
 
-    // Constructor không tham số
-    public Doctor() {
+    // ==== Constructor ====
+    // Tạo Constructor trống
+    public Doctor(){}
+
+    // Tạo constructor đủ thông tin
+    public Doctor(String doctorID, String name, String sex, String address, String departmentID, Date createDate, Date updateDate){
+        super(doctorID, name, createDate, updateDate);
+        this.departmentID = departmentID;
+        this.sex = sex;
+        this.address = address;
     }
 
-    @Override
-    public void showInfo() {
-        System.out.println(this.toString()); // In ra theo kiểu toString() được định nghĩa trong class này
+    // ==== Getter/Setter ====
+    // Getter
+    public String getDoctorID(){return getID();} // ID chính - để dễ gọi lại ID của doctor
+    public String getDoctorName(){return getName();} // Tên của Doctor
+    public String getSex(){return sex;}
+    public String getAddress(){return address;}
+    public String getDepartmentID(){return departmentID;} // DeptID của Doctor
+
+    // Setter
+    // docID không thể thay đổi -> private hoặc không có set
+    public void setSex(String sex){this.sex = sex;}
+    public void setAddress(String addr){this.address = addr;}
+    public void setDepartmentID(String deptID) {this.departmentID = deptID;} // deparmentID ở đây có thể sửa, vì doc có thể chuyển khoa -> cần có set
+
+    // ==== Hàm in ====
+    @Override public String toString(){
+        // Đặt biến chỉnh form ngày in
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String sdfCreateDate = (getCreateDate() != null) ? sdf.format(getCreateDate()) : "";
+        String sdfUpdateDate = (getLastUpdateDate() != null) ? sdf.format(getLastUpdateDate()) : "";
+
+        // Vẽ bảng in
+        return String.format("| %-15s | %-30s | %-7s | %-50s | %-15s | %-12s | %-12s |", getDoctorID(), getDoctorName(), sex, address, getDepartmentID(), sdfCreateDate, sdfUpdateDate);
     }
-    // Như vậy, giả sử đối tượng Department de = new Department() -> de.showInfo()
-    // sẽ in ra theo kiểu của class này
-    // Và Doctor do = new Doctor() -> do.showInfo() sẽ in ra theo kiểu bảng của
-    // class Doctor.
+
+    @Override public void showInfo(){
+        System.out.println(this.toString());
+    }
 }
--- src/main/java/hospital/IManager.java
+++ src/main/java/hospital/IManager.java
@@ -1,13 +1,18 @@
 package hospital;
 
-
-
 public interface IManager<T> {
+    // Không cần dùng public trước các hàm vì trong interface đã mặc định mọi phương thức là public adstract
+    // Thêm khoa mới (check departmentID không được trùng).
     boolean add(T item);
+    // Sửa thông tin khoa theo departmentID (khi sửa xong gán lastUpdateDate là ngày hiện tại; nếu nhập khoảng trắng thì giữ nguyên thông tin cũ).
     boolean update(T item);
+    // Xóa khoa (không được xóa nếu đang có Bác sĩ thuộc khoa đó)
     boolean delete(String id);
-    T findById(String id);
+    // Tìm khoa theo departmentID
+    T findByID(String id);
+    // In toàn bộ danh sách khoa.
     void showAll();
-    void loadFromFile(String path) throws Exception;
-    void saveToFile(String path) throws Exception;
+    // Ghi/Đọc file department.dat
+    void loadFromFile(String path);
+    void saveToFile(String path);
 }
