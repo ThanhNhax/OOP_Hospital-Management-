965f4e111d5f7a3097baa6a3bf887acf9a4d0994
philinh6000 <philinh6000@gmail.com>
2026-09-02 22:21:35 -0700

    Xong DepartmentManager va utils, chua co has_doctor, cho viet DoctorManager

---
+++ src/main/java/hospital/utils.java
@@ -0,0 +1,9 @@
+package hospital;
+
+
+public class utils {
+    // Dùng static để có thể gọi nhanh ở mọi nơi
+    public static void Notice(String text){
+        System.out.println(text);
+    }
+}
